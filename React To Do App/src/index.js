10a80c4e40564f60b6eefed01f748b9c9c695ac3
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-08-23 07:00:59 -0600

    Update index.js

--- React To Do App/src/index.js
+++ React To Do App/src/index.js
@@ -1,2 +1,40 @@
-const element = React.createElement('h1', null, 'React To-Do App');
-  ReactDOM.render(element, document.getElementById('root'));
+const App = ({ name }) => {
+  return (
+
+    <div>
+<h1>{name}</h1>
+<form>
+<label className="todo-label"> New name for</label>
+    <input type="text">
+    <button type="button">Cancel<span className="visually-hidden">renaming {props.name}</span></button> <button type="submit">
+          Save <span className="visually-hidden">new name for </span>
+        </button>
+</form>
+
+<input type="checkbox">
+  <label className="todo-label">
+   </label>
+  <button type="button">
+            Edit <span className="visually-hidden"></span>
+          </button>
+   <button
+            type="button">
+            Delete <span className="visually-hidden"></span>
+          </button>
+  
+  
+
+
+
+
+
+
+
+    </div>
+    );
+};
+
+ReactDOM.render(
+  <App name="To Do List" />,
+  document.getElementById("root")
+);
