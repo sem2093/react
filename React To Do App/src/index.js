5cb0fb7c5f55c16d3e1e7ae5982000817dd580bd
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-08-23 00:47:42 -0600

    Update index.js

--- React To Do App/src/index.js
+++ React To Do App/src/index.js
@@ -1,2 +1,2 @@
-const element = React.createElement('h1', null, 'Hello, React!');
+const element = React.createElement('h1', null, 'React To Do App');
   ReactDOM.render(element, document.getElementById('root'));
