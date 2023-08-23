a53c0b1262a01b8bb627382b610e9e494868a18e
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-08-23 02:34:01 -0600

    Update app.js

--- project-2/src/app.js
+++ project-2/src/app.js
@@ -1,5 +1,3 @@
-import React, { useState } from 'react'
-import "./App.css"
 import { numbers, upperCaseLetters, lowerCaseLetters, specialCharacters } from './Character'
 import { toast, ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
