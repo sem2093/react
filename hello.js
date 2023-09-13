19b51a7046de3083b2ad2bb3605311aae5c54c70
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-09-13 10:23:46 -0600

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,1 +1,7 @@
+function Hello() {
+        return <h1> React Project </h1>;
+      }
 
+      const container = document.getElementById('mydiv');
+      const root = ReactDOM.createRoot(container);
+      root.render(<Hello />)
